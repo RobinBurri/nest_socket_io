ff7cd1c3c78abd02c54f4f63d5168b2c21c4df99
Robin Burri <robin@Robins-MacBook-Air.local>
2023-08-01 09:42:45 +0200

    alerts working

--- src/alert/alert.controller.ts
+++ src/alert/alert.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, HttpCode } from '@nestjs/common';
+import { AlertGateway } from './alert.gateway';
 
 @Controller('alert')
-export class AlertController {}
+export class AlertController {
+  constructor(private alerteGateway: AlertGateway) {}
+
+  @Post()
+  @HttpCode(200)
+  sendAlertToAll(@Body() dto: { message: string }) {
+    this.alerteGateway.sendToAll(dto.message);
+    return dto;
+  }
+}
